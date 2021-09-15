07c53d8efd61b88b24a630ead66798efbe5ca6ed
Pavel Simonov <pavel.simonov@citeck.ru>
2021-09-15 08:18:43 +0700

    Fix roles mixin

--- ecos-model-lib/src/main/java/ru/citeck/ecos/model/lib/role/api/records/RolesMixin.kt
+++ ecos-model-lib/src/main/java/ru/citeck/ecos/model/lib/role/api/records/RolesMixin.kt
@@ -3,6 +3,7 @@ package ru.citeck.ecos.model.lib.role.api.records
 import ru.citeck.ecos.context.lib.auth.AuthContext
 import ru.citeck.ecos.model.lib.role.constants.RoleConstants
 import ru.citeck.ecos.model.lib.role.service.RoleService
+import ru.citeck.ecos.records2.RecordRef
 import ru.citeck.ecos.records3.record.atts.value.AttValue
 import ru.citeck.ecos.records3.record.atts.value.AttValueCtx
 import ru.citeck.ecos.records3.record.mixin.AttMixin
@@ -11,7 +12,10 @@ class RolesMixin(val service: RoleService) : AttMixin {
 
     override fun getAtt(path: String, value: AttValueCtx): Any? {
         if (path == RoleConstants.ATT_ROLES) {
-            return RolesAttValue(value)
+            // getRef() should be called on the top of mixin
+            // attribute to avoid using sourceIdMapping if we
+            // work with target RecordsDao through RecordsDaoProxy
+            return RolesAttValue(value.getRef())
         }
         return null
     }
@@ -20,31 +24,31 @@ class RolesMixin(val service: RoleService) : AttMixin {
         return listOf(RoleConstants.ATT_ROLES)
     }
 
-    inner class RolesAttValue(private val value: AttValueCtx) : AttValue {
+    inner class RolesAttValue(private val valueRef: RecordRef) : AttValue {
 
         override fun getAtt(name: String?): Any? {
             return when (name) {
-                RoleConstants.ATT_IS_CURRENT_USER_MEMBER_OF -> IsMemberOfRoleValue(value)
-                RoleConstants.ATT_ASSIGNEES_OF -> AssigneesOfRoleValue(value)
+                RoleConstants.ATT_IS_CURRENT_USER_MEMBER_OF -> IsMemberOfRoleValue(valueRef)
+                RoleConstants.ATT_ASSIGNEES_OF -> AssigneesOfRoleValue(valueRef)
                 else -> null
             }
         }
     }
 
-    inner class AssigneesOfRoleValue(private val value: AttValueCtx) : AttValue {
+    inner class AssigneesOfRoleValue(private val valueRef: RecordRef) : AttValue {
 
         override fun getAtt(roleName: String): Any {
             return AuthContext.runAsSystem {
-                service.getAssignees(value.getRef(), roleName)
+                service.getAssignees(valueRef, roleName)
             }
         }
     }
 
-    inner class IsMemberOfRoleValue(private val value: AttValueCtx) : AttValue {
+    inner class IsMemberOfRoleValue(private val valueRef: RecordRef) : AttValue {
 
         override fun getAtt(roleName: String): Boolean {
             return AuthContext.runAsSystem {
-                service.isRoleMember(value.getRef(), roleName)
+                service.isRoleMember(valueRef, roleName)
             }
         }
     }
