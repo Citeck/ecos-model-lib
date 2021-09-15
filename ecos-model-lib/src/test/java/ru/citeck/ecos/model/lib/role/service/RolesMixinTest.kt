af166bd654e9c46412d0e41e75aefb87c28607fe
Pavel Simonov <pavel.simonov@citeck.ru>
2021-09-15 08:46:38 +0700

    Add computed roles mixin test

--- ecos-model-lib/src/test/java/ru/citeck/ecos/model/lib/role/service/RolesMixinTest.kt
+++ ecos-model-lib/src/test/java/ru/citeck/ecos/model/lib/role/service/RolesMixinTest.kt
@@ -3,9 +3,11 @@ package ru.citeck.ecos.model.lib.role.service
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import ru.citeck.ecos.commons.data.MLText
+import ru.citeck.ecos.commons.data.ObjectData
 import ru.citeck.ecos.context.lib.auth.AuthContext
 import ru.citeck.ecos.model.lib.ModelServiceFactory
 import ru.citeck.ecos.model.lib.role.api.records.RolesMixin
+import ru.citeck.ecos.model.lib.role.dto.RoleComputedDef
 import ru.citeck.ecos.model.lib.role.dto.RoleDef
 import ru.citeck.ecos.model.lib.type.dto.TypeModelDef
 import ru.citeck.ecos.model.lib.type.repo.TypesRepo
@@ -13,7 +15,9 @@ import ru.citeck.ecos.model.lib.type.service.utils.TypeUtils
 import ru.citeck.ecos.records2.RecordRef
 import ru.citeck.ecos.records2.source.dao.local.RecordsDaoBuilder
 import ru.citeck.ecos.records3.RecordsServiceFactory
+import ru.citeck.ecos.records3.record.atts.computed.ComputedAttType
 import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
+import ru.citeck.ecos.records3.record.dao.impl.proxy.RecordsDaoProxy
 
 class RolesMixinTest {
 
@@ -26,6 +30,12 @@ class RolesMixinTest {
 
         val roleWithUserId = "roleWithUser"
         val roleWithUserAssignees = listOf("user0")
+        val computedRoleId = "computed-role"
+        val computedRoleAssignees = listOf("GROUP_first", "GROUP_second")
+        val computedRoleConfigJson = """{"fn":"
+            |var strVal = value.load('strAtt');
+            |return ['${computedRoleAssignees.joinToString("','")}', strVal];
+        |"}""".trimMargin().replace("\n", " ")
 
         val services = object : ModelServiceFactory() {
             override fun createTypesRepo(): TypesRepo {
@@ -44,6 +54,14 @@ class RolesMixinTest {
                                         id = roleWithUserId
                                         name = MLText(roleWithUserId)
                                         assignees = roleWithUserAssignees
+                                    },
+                                    RoleDef.create {
+                                        id = computedRoleId
+                                        name = MLText(computedRoleId)
+                                        computed = RoleComputedDef.create {
+                                            withType(ComputedAttType.SCRIPT)
+                                            withConfig(ObjectData.create(computedRoleConfigJson))
+                                        }
                                     }
                                 )
                             }
@@ -63,11 +81,17 @@ class RolesMixinTest {
         }
 
         val recId = "rec-0"
-        val recRef = RecordRef.create("test", recId)
+        val sourceId = "test"
+        val proxyId = "test-proxy"
+        val recRefs = listOf(
+            RecordRef.create(sourceId, recId),
+            RecordRef.create(proxyId, recId)
+        )
 
         val recordsServices = RecordsServiceFactory()
         val records = recordsServices.recordsServiceV1
         services.setRecordsServices(recordsServices)
+        records.register(RecordsDaoProxy(proxyId, sourceId))
 
         val recsDao = RecordsDaoBuilder.create("test")
             .addRecord(recId, TestDto(testTypeRef))
@@ -75,17 +99,25 @@ class RolesMixinTest {
         (recsDao as AbstractRecordsDao).addAttributesMixin(RolesMixin(services.roleService))
         records.register(recsDao)
 
-        assertThat(records.getAtt(recRef, "_roles.isCurrentUserMemberOf.$roleId?bool").asBoolean()).isFalse
+        for (recRef in recRefs) {
 
-        AuthContext.runAs("user0", listOf("GROUP_EXP_FIRST")) {
-            assertThat(records.getAtt(recRef, "_roles.isCurrentUserMemberOf.$roleId?bool").asBoolean()).isTrue
-        }
-        AuthContext.runAs("user0", emptyList()) {
-            assertThat(records.getAtt(recRef, "_roles.isCurrentUserMemberOf.$roleWithUserId?bool").asBoolean()).isTrue
-        }
+            assertThat(records.getAtt(recRef, "_roles.isCurrentUserMemberOf.$roleId?bool").asBoolean()).isFalse
 
-        assertThat(records.getAtt(recRef, "_roles.assigneesOf.$roleId[]?str").asStrList())
-            .containsExactlyElementsOf(explicitAssignees)
+            AuthContext.runAs("user0", listOf("GROUP_EXP_FIRST")) {
+                assertThat(records.getAtt(recRef, "_roles.isCurrentUserMemberOf.$roleId?bool").asBoolean()).isTrue
+            }
+            AuthContext.runAs("user0", emptyList()) {
+                assertThat(
+                    records.getAtt(recRef, "_roles.isCurrentUserMemberOf.$roleWithUserId?bool").asBoolean()
+                ).isTrue
+            }
+
+            assertThat(records.getAtt(recRef, "_roles.assigneesOf.$roleId[]?str").asStrList())
+                .containsExactlyElementsOf(explicitAssignees)
+
+            val computedList = records.getAtt(recRef, "_roles.assigneesOf.$computedRoleId[]?str").asStrList()
+            assertThat(computedList).containsExactly(*computedRoleAssignees.toTypedArray(), "str-value")
+        }
     }
 
     data class TestDto(
@@ -94,5 +126,7 @@ class RolesMixinTest {
         fun getEcosType(): RecordRef {
             return typeRef
         }
+
+        fun getStrAtt(): String = "str-value"
     }
 }
