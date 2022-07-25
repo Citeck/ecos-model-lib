dad4b6592979372cd07811c963c36dfc933c865b
Pavel Simonov <pavel.simonov@citeck.ru>
2022-07-25 08:59:32 +0700

    ECOSENT-2037 - Add GROUP_EVERYONE support

--- ecos-model-lib/src/main/java/ru/citeck/ecos/model/lib/role/service/RoleService.kt
+++ ecos-model-lib/src/main/java/ru/citeck/ecos/model/lib/role/service/RoleService.kt
@@ -1,6 +1,7 @@
 package ru.citeck.ecos.model.lib.role.service
 
 import ru.citeck.ecos.context.lib.auth.AuthContext
+import ru.citeck.ecos.context.lib.auth.AuthGroup
 import ru.citeck.ecos.model.lib.ModelServiceFactory
 import ru.citeck.ecos.model.lib.ModelServiceFactoryAware
 import ru.citeck.ecos.model.lib.attributes.dto.computed.ComputedAttType
@@ -32,6 +33,39 @@ class RoleService : ModelServiceFactoryAware {
         services.records.globalAttMixinsProvider.addMixin(RolesMixin(this))
     }
 
+    fun getCurrentUserRoles(record: Any?): List<String> {
+        return getCurrentUserRoles(record, typeRefService.getTypeRef(record))
+    }
+
+    fun getCurrentUserRoles(record: Any?, typeRef: RecordRef?): List<String> {
+        return getRolesForAuthorities(record, typeRef, AuthContext.getCurrentUserWithAuthorities())
+    }
+
+    fun getRolesForAuthorities(record: Any?, authorities: Collection<String>?): List<String> {
+        return getRolesForAuthorities(record, typeRefService.getTypeRef(record), authorities)
+    }
+
+    fun getRolesForAuthorities(record: Any?, typeRef: RecordRef?, authorities: Collection<String>?): List<String> {
+
+        record ?: return emptyList()
+
+        val roles = getRolesId(typeRef)
+        if (roles.isEmpty()) {
+            return emptyList()
+        }
+
+        val assigneesByRoleId = getAssignees(record, typeRef, roles)
+        val currentUserAuthorities = authorities?.toSet() ?: emptySet()
+
+        val result = mutableListOf<String>()
+        assigneesByRoleId.forEach { (roleId, assignees) ->
+            if (roleId == RoleConstants.ROLE_EVERYONE || assignees.any { currentUserAuthorities.contains(it) }) {
+                result.add(roleId)
+            }
+        }
+        return result
+    }
+
     fun getRolesId(typeRef: RecordRef?): List<String> {
         return getRoles(typeRef).map { it.id }
     }
@@ -54,87 +88,123 @@ class RoleService : ModelServiceFactoryAware {
         return assignees.any { currentUserAuthorities.contains(it) }
     }
 
+    fun getAssignees(record: Any?, roles: Collection<String>?): Map<String, List<String>> {
+        return getAssignees(record, typeRefService.getTypeRef(record), roles)
+    }
+
     fun getAssignees(record: Any?, roleId: String?): List<String> {
-
-        roleId ?: return emptyList()
-        record ?: return emptyList()
-
         return getAssignees(record, typeRefService.getTypeRef(record), roleId)
     }
 
     fun getAssignees(record: Any?, typeRef: RecordRef?, roleId: String?): List<String> {
-
-        record ?: return emptyList()
-        typeRef ?: return emptyList()
-
-        if (roleId == RoleConstants.ROLE_EVERYONE) {
-            // Role 'EVERYONE' is virtual and doesn't have assignees
+        if (record == null || typeRef == null || roleId.isNullOrBlank()) {
             return emptyList()
+        }
+        return getAssignees(record, typeRef, listOf(roleId)).getOrDefault(roleId, emptyList())
+    }
+
+    fun getAssignees(record: Any?, typeRef: RecordRef?, roles: Collection<String>?): Map<String, List<String>> {
+
+        if (roles.isNullOrEmpty() || record == null || typeRef == null || typeRef.id.isBlank()) {
+            return roles?.associateWith { emptyList() } ?: emptyMap()
+        }
+
+        val rolesToEval = roles.toMutableSet()
+        val result = LinkedHashMap<String, List<String>>()
+
+        val it = rolesToEval.iterator()
+        while (it.hasNext()) {
+            val roleId = it.next()
+            if (roleId == RoleConstants.ROLE_EVERYONE) {
+                // Role 'EVERYONE' should always contain GROUP_EVERYONE
+                result[roleId] = arrayListOf(AuthGroup.EVERYONE)
+                it.remove()
+            } else if (roleId.isBlank()) {
+                result[roleId] = emptyList()
+                it.remove()
+            }
+        }
+        if (rolesToEval.isEmpty()) {
+            return result
         }
 
         if (record is RecordRef && record.appName.isNotEmpty() && record.appName != currentAppName) {
-            return recordsService.getAtt(
-                record,
-                RoleConstants.ATT_ROLES + "." + RoleConstants.ATT_ASSIGNEES_OF + ".$roleId[]?str"
-            ).asStrList()
+            val assigneesAtts = rolesToEval.associateWith {
+                RoleConstants.ATT_ROLES + "." + RoleConstants.ATT_ASSIGNEES_OF + ".$it[]?str"
+            }
+            recordsService.getAtts(record, assigneesAtts).forEach { roleId, assignees ->
+                result[roleId] = assignees.asStrList()
+            }
+            return result
         }
 
-        val roleDef = getRoleDef(typeRef, roleId)
-        if (roleDef.id.isBlank()) {
-            return emptyList()
+        val roleDefById = getRoles(typeRef).associateBy { it.id }
+
+        val rolesDef = rolesToEval.mapNotNull {
+            val def = roleDefById[it]
+            if (def == null) {
+                result[it] = emptyList()
+            }
+            def
         }
-        val roleAtts = roleDef.attributes
-        val assignees: MutableList<String> = ArrayList()
 
-        if (roleAtts.isNotEmpty()) {
+        // todo: join record attributes calculation
+        rolesDef.forEach { roleDef ->
 
-            val atts = roleAtts.associateWith { "$it[]?str" }
-            val attsValues = recordsService.getAtts(record, atts)
+            val roleAtts = roleDef.attributes
+            val assignees: MutableList<String> = ArrayList()
 
-            roleAtts.forEach {
-                val value = attsValues.getAtt(it)
-                if (value.isArray()) {
-                    assignees.addAll(value.asStrList())
+            if (roleAtts.isNotEmpty()) {
+
+                val atts = roleAtts.associateWith { "$it[]?str" }
+                val attsValues = recordsService.getAtts(record, atts)
+
+                roleAtts.forEach {
+                    val value = attsValues.getAtt(it)
+                    if (value.isArray()) {
+                        assignees.addAll(value.asStrList())
+                    }
                 }
             }
-        }
-        assignees.addAll(roleDef.assignees)
+            assignees.addAll(roleDef.assignees)
 
-        val computed = roleDef.computed
+            val computed = roleDef.computed
 
-        if (computed.type == ComputedAttType.SCRIPT ||
-            computed.type == ComputedAttType.VALUE ||
-            computed.type == ComputedAttType.ATTRIBUTE ||
-            computed.type == ComputedAttType.TEMPLATE
-        ) {
+            if (computed.type == ComputedAttType.SCRIPT ||
+                computed.type == ComputedAttType.VALUE ||
+                computed.type == ComputedAttType.ATTRIBUTE ||
+                computed.type == ComputedAttType.TEMPLATE
+            ) {
 
-            val computedAttValue = RecordComputedAttValue.create()
-                .withType(computed.type.toRecordComputedType())
-                .withConfig(computed.config)
-                .build()
+                val computedAttValue = RecordComputedAttValue.create()
+                    .withType(computed.type.toRecordComputedType())
+                    .withConfig(computed.config)
+                    .build()
 
-            val authorities = RequestContext.doWithAtts(mapOf("roleAtt" to computedAttValue)) { _ ->
-                recordsService.getAtt(record, "\$roleAtt[]?str").asStrList()
+                val authorities = RequestContext.doWithAtts(mapOf("roleAtt" to computedAttValue)) { _ ->
+                    recordsService.getAtt(record, "\$roleAtt[]?str").asStrList()
+                }
+                assignees.addAll(authorities)
             }
-            assignees.addAll(authorities)
-        }
 
-        val assigneesSet = HashSet<String>()
-        val uniqueAssignees = assignees.map { it.trim() }.filter { it.isNotBlank() && assigneesSet.add(it) }
+            val assigneesSet = HashSet<String>()
+            val uniqueAssignees = assignees.map { it.trim() }.filter { it.isNotBlank() && assigneesSet.add(it) }
 
-        val names = authorityService?.getAuthorityNames(uniqueAssignees) ?: uniqueAssignees
+            val names = authorityService?.getAuthorityNames(uniqueAssignees) ?: uniqueAssignees
 
-        if (names.size != uniqueAssignees.size) {
-            error(
-                "Authority component should return list with same length from method getAuthorityNames. " +
-                    "Actual names: $names Argument names: $uniqueAssignees"
-            )
+            if (names.size != uniqueAssignees.size) {
+                error(
+                    "Authority component should return list with same length from method getAuthorityNames. " +
+                        "Actual names: $names Argument names: $uniqueAssignees"
+                )
+            }
+            if (names === uniqueAssignees) {
+                result[roleDef.id] = names
+            }
+            assigneesSet.clear()
+            result[roleDef.id] = names.map { it.trim() }.filter { it.isNotBlank() && assigneesSet.add(it) }
         }
-        if (names === uniqueAssignees) {
-            return names
-        }
-        assigneesSet.clear()
-        return names.map { it.trim() }.filter { it.isNotBlank() && assigneesSet.add(it) }
+        return result
     }
 
     fun getRoleDef(typeRef: RecordRef?, roleId: String?): RoleDef {
